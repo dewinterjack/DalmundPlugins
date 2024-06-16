b316159daa3f9250c3308d31fb2e221f4c748ad3
Jack Dewinter <jackdewinter1@gmail.com>
2024-06-16 04:46:35 +0100

    initial commit

---
+++ WhereTheSquadAt/Plugin.cs
@@ -0,0 +1,89 @@
+﻿using Dalamud.Game.Command;
+using Dalamud.IoC;
+using Dalamud.Plugin;
+using System.IO;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+using WhereTheSquadAt.Windows;
+
+namespace WhereTheSquadAt;
+
+public sealed class Plugin : IDalamudPlugin
+{
+    private const string CommandName = "/pmycommand";
+
+    private DalamudPluginInterface PluginInterface { get; init; }
+    private ICommandManager CommandManager { get; init; }
+    public Configuration Configuration { get; init; }
+
+    public readonly WindowSystem WindowSystem = new("WhereTheSquadAt");
+    private ConfigWindow ConfigWindow { get; init; }
+    private MainWindow MainWindow { get; init; }
+
+    private IPartyList party;
+    private IClientState client;
+
+    public Plugin(
+        [RequiredVersion("1.0")] DalamudPluginInterface pluginInterface,
+        [RequiredVersion("1.0")] ICommandManager commandManager,
+        [RequiredVersion("1.0")] ITextureProvider textureProvider,
+        [RequiredVersion("1.0")] IPartyList partyList,
+        [RequiredVersion("1.0")] IClientState clientState)
+    {
+        PluginInterface = pluginInterface;
+        CommandManager = commandManager;
+        party = partyList;
+        client = clientState;
+
+        Configuration = PluginInterface.GetPluginConfig() as Configuration ?? new Configuration();
+        Configuration.Initialize(PluginInterface);
+
+
+        // you might normally want to embed resources and load them from the manifest stream
+        var file = new FileInfo(Path.Combine(PluginInterface.AssemblyLocation.Directory?.FullName!, "goat.png"));
+
+        // ITextureProvider takes care of the image caching and dispose
+        var goatImage = textureProvider.GetTextureFromFile(file);
+
+        ConfigWindow = new ConfigWindow(this);
+        MainWindow = new MainWindow(this, goatImage, party, client);
+
+        WindowSystem.AddWindow(ConfigWindow);
+        WindowSystem.AddWindow(MainWindow);
+
+        CommandManager.AddHandler(CommandName, new CommandInfo(OnCommand)
+        {
+            HelpMessage = "A useful message to display in /xlhelp"
+        });
+
+        PluginInterface.UiBuilder.Draw += DrawUI;
+
+        // This adds a button to the plugin installer entry of this plugin which allows
+        // to toggle the display status of the configuration ui
+        PluginInterface.UiBuilder.OpenConfigUi += ToggleConfigUI;
+
+        // Adds another button that is doing the same but for the main ui of the plugin
+        PluginInterface.UiBuilder.OpenMainUi += ToggleMainUI;
+    }
+
+    public void Dispose()
+    {
+        WindowSystem.RemoveAllWindows();
+
+        ConfigWindow.Dispose();
+        MainWindow.Dispose();
+
+        CommandManager.RemoveHandler(CommandName);
+    }
+
+    private void OnCommand(string command, string args)
+    {
+        // in response to the slash command, just toggle the display status of our main ui
+        ToggleMainUI();
+    }
+
+    private void DrawUI() => WindowSystem.Draw();
+
+    public void ToggleConfigUI() => ConfigWindow.Toggle();
+    public void ToggleMainUI() => MainWindow.Toggle();
+}
---
+++ WhereTheSquadAt/Windows/MainWindow.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Numerics;
+using Dalamud.Interface.Internal;
+using Dalamud.Interface.Utility;
+using Dalamud.Interface.Windowing;
+using Dalamud.Plugin.Services;
+using ImGuiNET;
+using Dalamud.Game.ClientState.Objects.SubKinds;
+
+
+namespace WhereTheSquadAt.Windows;
+
+public class MainWindow : Window, IDisposable
+{
+    private IDalamudTextureWrap? GoatImage;
+    private Plugin Plugin;
+
+    private IPartyList PartyList;
+    private IClientState Client;
+
+    // We give this window a hidden ID using ##
+    // So that the user will see "My Amazing Window" as window title,
+    // but for ImGui the ID is "My Amazing Window##With a hidden ID"
+    public MainWindow(Plugin plugin, IDalamudTextureWrap? goatImage, IPartyList partyList, IClientState client)
+        : base("My Amazing Window##With a hidden ID", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
+    {
+        SizeConstraints = new WindowSizeConstraints
+        {
+            MinimumSize = new Vector2(375, 330),
+            MaximumSize = new Vector2(float.MaxValue, float.MaxValue)
+        };
+
+        GoatImage = goatImage;
+        Plugin = plugin;
+        PartyList = partyList;
+        Client = client;
+    }
+
+    public void Dispose() { }
+
+    public override void Draw()
+    {
+        if (ImGui.Begin("Party Locations"))
+        {
+            var playerPosition = Client.LocalPlayer?.Position ?? Vector3.Zero;
+            foreach (var member in PartyList)
+            {
+                var player = member.GameObject as PlayerCharacter;
+                if (player != null)
+                {
+                    var partyMemberPosition = player.Position;
+                    var distance = Vector3.Distance(playerPosition, partyMemberPosition);
+                    ImGui.Text($"{player.Name}: {player.CurrentWorld.GameData.Name} {distance}");
+                }
+            }
+            ImGui.End();
+        }
+        ImGui.Text($"The randoms config bool is {Plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
+
+        if (ImGui.Button("Show Settings"))
+        {
+            Plugin.ToggleConfigUI();
+        }
+
+        ImGui.Spacing();
+
+        ImGui.Text("Have a goat:");
+        if (GoatImage != null)
+        {
+            ImGuiHelpers.ScaledIndent(55f);
+            ImGui.Image(GoatImage.ImGuiHandle, new Vector2(GoatImage.Width, GoatImage.Height));
+            ImGuiHelpers.ScaledIndent(-55f);
+        }
+        else
+        {
+            ImGui.Text("Image not found.");
+        }
+    }
+}
